0c771fac8bab6eca87ed4262e9c749873ada0348
Khalid <Khalidbelhadj124@gmail.com>
2024-08-07 00:01:13 +0100

    Fix libs missing

--- src/components/ImageCarouselHelper.tsx
+++ src/components/ImageCarouselHelper.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { cn } from "../../@/lib/utils";
+import { cn } from "../utils";
 
 export default function ImageCarouselHelper({ images }: { images: string[] }) {
   const [currentImage, setCurrentImage] = useState(0);
