448984ddbc826b525a8cd6c745d4113fff3d7cd7
Khalid <Khalidbelhadj124@gmail.com>
2024-08-06 22:54:09 +0100

    Initial commit

---
+++ src/components/ImageCarouselHelper.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { cn } from "../../@/lib/utils";
+
+export default function ImageCarouselHelper({ images }: { images: string[] }) {
+  const [currentImage, setCurrentImage] = useState(0);
+  console.log(images[currentImage]);
+
+  return (
+    <>
+      {images.map((image, index) => (
+        <div
+          className={cn(
+            "bg-red-100 w-full h-full absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center transition-opacity duration-300 ease-in-out opacity-0",
+            currentImage === index && "opacity-100"
+          )}
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        ></div>
+      ))}
+      <div className="flex gap-3 items-center justify-center pb-2">
+        {images.length > 1 &&
+          images.map((_, index) => (
+            <button
+              className={cn(
+                "z-10 bg-accent/80 rounded-full w-4 h-4 transition-all border-[1px] border-foreground/20",
+                currentImage === index && "bg-accent/50"
+              )}
+              onClick={() => setCurrentImage(index)}
+            ></button>
+          ))}
+      </div>
+    </>
+  );
+}
---
+++ src/consts.ts
@@ -0,0 +1,49 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Khalid Belhadj";
+export const SITE_DESCRIPTION = "Khalid's personal portfolio";
+export const DEFAULT_LOCATION = "edinburgh, united kingdom";
+export const LOCATION_CHANNEL =
+  "https://api.thingspeak.com/channels/2616056/feeds.json";
+
+export const about =
+  "I'm a software engineer interested in machine learning, date science and product development. I'm currently in my 3rd year, studying AI and Computer Science at The University of Edinburgh. This website is for me to share my projects, ideas and interests.";
+
+export const education = [
+  {
+    dates: "2021 - 2025",
+    description:
+      "Artificial Intelligence and Computer Science (BSc Hons) at The University of Edinburgh",
+  },
+  {
+    dates: "2019 - 2021",
+    description: "International Baccalaureate at Uptown School Dubai",
+  },
+];
+
+export const experience = [
+  {
+    dates: "2024",
+    description: "Morgan Stanley, Technology Summer Analyst",
+  },
+  {
+    dates: "2023 - 2024",
+    description: "The University of Edinburgh, InfBase Tutoring",
+  },
+  {
+    dates: "2023",
+    description: "BennuAI, Software Development Contractor",
+  },
+];
+
+export const skills = [
+  {
+    type: "programming",
+    values: ["C/C++", "Java", "Rust", "Typescript", "React"],
+  },
+  {
+    type: "languages",
+    values: ["English", "Arabic", "French (Intermediate)"],
+  },
+];
