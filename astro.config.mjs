be86307dd59e27b706325ddde437e4e9338dfa9d
Khalid <Khalidbelhadj124@gmail.com>
2024-09-18 09:25:17 +0100

    Change base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://khalidbelhadj.github.io",
-  base: "portfolio",
+  site: "https://khalidbelhadj.com",
+  base: "/",
   output: "static",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
