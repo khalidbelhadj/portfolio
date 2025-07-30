b8d8271491304a7d86ba0c6747233307c555fc2a
Khalid Belhadj <khalidbelhadj124@gmail.com>
2025-07-30 13:24:28 +0100

    remove light font

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,6 @@ import { z, defineCollection } from "astro:content";
 const posts = defineCollection({
   type: "content",
   schema: z.object({
-    id: z.string(),
     title: z.string(),
     date: z.date(),
   }),
